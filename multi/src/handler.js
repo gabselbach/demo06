f2904dd3a38be911fca0cca751d5695ec85972f6
Gabriella <gabriellaselbach@gmail.com>
2023-08-28 09:54:32 -0300

    first commit

--- multi/src/handler.js
+++ multi/src/handler.js
@@ -1,8 +1,6 @@
 const settings = require("./../config/settings")
 const axios = require("axios")
 const cheerio = require("cheerio")
-const { dynamoDB } = require("./factory")
-const { randomUUID } = require('node:crypto')
 
 class Handler {
   static async main(event) {
@@ -11,19 +9,15 @@ class Handler {
     const $ = cheerio.load(data)
     const [commitMessage] = await $('#content').text().trim().split('\n')
     console.log('Message', commitMessage )
-    const params = {
-      TableName: settings.dbTableName,
-      Item: {
+    const body = {
         commitMessage,
-        id: randomUUID(),
         createdAt: new Date().toISOString()
-      }
     }
-    await dynamoDB.put(params).promise()
     console.log('process finished at', new Date().toISOString())
 
     return {
-      statusCode: 200
+      statusCode: 200,
+      body
     }
   }
 }
